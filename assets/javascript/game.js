39775e58d6a7ff9763c3f6aaf56ca6f0c0fd5977
Melinda Santana <melinda.santana@gmail.com>
2019-07-08 22:22:09 -0700

    Adjusting variables and splitting code into a way that is a little less confusing to follow
    Adjusting variables and splitting code into a way that is a little less confusing to follow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
-var choices = ["a","b","c","d","e"]
+var choices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var computerGuess;  
-var GuessesRemaining = 9
+var GuessesRemaining = 9;
+var letters = [];
 
 function KeyPress (){
     console.log ("a key was pressed")
@@ -11,6 +12,8 @@ document.onkeydown = function (event) {
     console.log (event.key)
     if (event.key == computerGuess) {
         console.log ("You Win!")
+        win++;
+        document.querySelector(#wins).innerHTML = "Wins:" + win;
     } else {
         if (GuessesRemaining === 1) {
             console.log ("You Lose!")
@@ -18,14 +21,14 @@ document.onkeydown = function (event) {
             console.log ("Incorrect Guess")
             GuessesRemaining = GuessesRemaining - 1
             console.log (GuessesRemaining)
-document.querySelector ("#GuessesLeft").innerHTML = "Guesses Left: " + GuessesRemaining
+document.querySelector (#GuessesLeft).innerHTML = "Guesses Left: " + GuessesRemaining
         }
-    }
+
 }
 
 
 function PickRandomLetter (){
-    var RandomNumber = (Math.floor(Math.random()*5))
+    var RandomNumber = (Math.floor(Math.random()*choices.length))
     console.log ("The computer chose " + choices [RandomNumber])
     computerGuess = choices [RandomNumber]
 }
