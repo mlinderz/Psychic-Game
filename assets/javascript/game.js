b0d75600bba1a5c1a219b1c47c48f54fa20e0aa1
Melinda Santana <melinda.santana@gmail.com>
2019-07-10 19:08:34 -0700

    Reverting back the code
    I broke it - so I needed to go back to when it last worked :(

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
-var choices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var choices = ["a","b","c","d","e"]
 var computerGuess;  
-var GuessesRemaining = 9;
-var letters = [];
+var GuessesRemaining = 9
 
 function KeyPress (){
     console.log ("a key was pressed")
@@ -12,8 +11,6 @@ document.onkeydown = function (event) {
     console.log (event.key)
     if (event.key == computerGuess) {
         console.log ("You Win!")
-        win++;
-        document.querySelector(#wins).innerHTML = "Wins:" + win;
     } else {
         if (GuessesRemaining === 1) {
             console.log ("You Lose!")
@@ -21,15 +18,14 @@ document.onkeydown = function (event) {
             console.log ("Incorrect Guess")
             GuessesRemaining = GuessesRemaining - 1
             console.log (GuessesRemaining)
-document.querySelector (#GuessesLeft).innerHTML = "Guesses Left: " + GuessesRemaining
+document.querySelector ("#GuessesLeft").innerHTML = "Guesses Left: " + GuessesRemaining
         }
-
+    }
 }
 
 
 function PickRandomLetter (){
-    var RandomNumber = (Math.floor(Math.random()*choices.length))
+    var RandomNumber = (Math.floor(Math.random()*5))
     console.log ("The computer chose " + choices [RandomNumber])
     computerGuess = choices [RandomNumber]
 }
-
