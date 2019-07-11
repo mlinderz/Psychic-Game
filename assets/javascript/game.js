f8e9cfd3db2795a5c4f6564e220f93061ddac53d
Melinda Santana <melinda.santana@gmail.com>
2019-07-10 19:26:35 -0700

    Wins is working
    Need to reset once the code hits win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 var choices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var computerGuess;  
 var GuessesRemaining = 9
+var guessedLetters = []
+var wins = 0
 
 function KeyPress (){
     console.log ("a key was pressed")
@@ -11,6 +13,8 @@ document.onkeydown = function (event) {
     console.log (event.key)
     if (event.key == computerGuess) {
         console.log ("You Win!")
+        wins = wins + 1 
+        document.querySelector ("#Wins").innerHTML = "Wins: " + wins
     } else {
         if (GuessesRemaining === 1) {
             console.log ("You Lose!")
@@ -20,12 +24,14 @@ document.onkeydown = function (event) {
             console.log (GuessesRemaining)
 document.querySelector ("#GuessesLeft").innerHTML = "Guesses Left: " + GuessesRemaining
         }
+    
     }
+
 }
 
 
 function PickRandomLetter (){
-    var RandomNumber = (Math.floor(Math.random()*5))
+    var RandomNumber = (Math.floor(Math.random()*26))
     console.log ("The computer chose " + choices [RandomNumber])
     computerGuess = choices [RandomNumber]
 }
